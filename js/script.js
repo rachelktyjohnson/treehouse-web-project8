cc36cd731a841409368643d4ef30084dc40fd7f0
Rachel Johnson <rachelktyjohnson@gmail.com>
2021-07-17 20:28:17 +1000

    modal shows clicked on person's data

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const card_container = document.getElementById('card_container');
 const search_bar = document.getElementById('searchbar');
 const modal = document.getElementById('modal');
 const modal_btn_close = document.querySelector('.modal-box .close');
+let people_data = null;
 
 //try-block to get from url and turn into json, used for everything?
 async function extractJSON(url){
@@ -17,17 +18,17 @@ async function extractJSON(url){
 
 async function getPeople(url){
     const peopleJSON = await extractJSON(url);
-    //people_data = peopleJSON.results;
+    people_data = peopleJSON.results;
     return (peopleJSON);
 }
 
 function generateHTML(data){
     card_container.innerHTML="";
-    data.results.map( person =>{
+    data.results.map( (person, index) =>{
         const new_card = `
-        <div class="card">
+        <div class="card" data-index="${index}">
             <img src="${person.picture.medium}"/>
-            <div class="details">
+            <div class="details" data-index="${index}">
                 <h2>${person.name.first} ${person.name.last}</h2>
                 <p class="email">${person.email}</p>
                 <p class="city">${person.location.city}, ${person.location.state}</p>
@@ -57,7 +58,6 @@ search_bar.addEventListener('keyup', ()=>{
     let count = 0;
     for (let i=0; i<all_cards.length; i++){
         count += 1;
-        console.log(all_cards[i].textContent)
         if (all_cards[i].textContent.includes(search_value)){
             all_cards[i].style.display = "flex";
         } else {
@@ -66,7 +66,37 @@ search_bar.addEventListener('keyup', ()=>{
     }
 })
 
-//modal.style.display = "none";
+modal.style.display = "none";
 modal_btn_close.addEventListener('click', ()=>{
     modal.style.display="none";
 })
+
+card_container.addEventListener('click', (e)=>{
+    if(e.target.className!=="cards"){
+        let card = e.target;
+        if (e.target.className!=="card"){
+            card = card.parentNode
+        }
+        let index = card.dataset.index;
+        openModal(index)
+    }
+})
+
+function openModal(index){
+    let data = people_data[index];
+    modal.style.display = "flex";
+    console.log(data);
+    document.querySelector('.m-photo').src = data.picture.large;
+    document.querySelector('.m-name').innerHTML = `${data.name.first} ${data.name.last}`;
+    document.querySelector('.m-email').innerHTML = data.email;
+    document.querySelector('.m-city').innerHTML = `${data.location.city}, ${data.location.state}`;
+    let phone = data.phone.replaceAll("-","");
+    let beginning = phone.slice(0,3);
+    let middle = phone.slice(3,6);
+    let end = phone.slice(-4);
+    document.querySelector('.m-number').innerHTML = `(${beginning}) ${middle}-${end}`;
+    document.querySelector('.m-address').innerHTML = `${data.location.street.number} ${data.location.street.name}, ${data.location.city} ${data.location.postcode}`
+    let birthday_datetime = new Date(data.dob.date);
+    let birthday_string = `${birthday_datetime.getMonth()+1}/${birthday_datetime.getDate()}/${birthday_datetime.getFullYear()}`;
+    document.querySelector('.m-dob').innerHTML = `Birthday: ${birthday_string}`;
+}
